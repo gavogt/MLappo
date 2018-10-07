be69e705146ed58af624ee1a9801195d4ba4ce31
gavogt <gvogt@asu.edu>
2018-10-06 17:31:22 -0700

    Initialized a new pipline and setting up the pipline for reading the CSV

--- Malappo/Program.cs
+++ Malappo/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Malappo
+﻿using Microsoft.ML.Legacy;
+using Microsoft.ML.Legacy.Data;
+
+namespace Malappo
 {
     class Program
     {
         static void Main(string[] args)
         {
             // Create a pipeline and load data
+            var pipeline = new LearningPipeline();
+
+            // Set the path to the file
+            string data = "BlackFriday.txt";
+            pipeline.Add(new TextLoader(data).CreateFrom<BlackFridayData>(separator: ','));
 
         }
     }
